544e386ca519dbfa436df9aaf1693fd8c8310cec
Alex Yeh <111445339+AlexYessirski@users.noreply.github.com>
2025-01-02 01:27:19 -0800

    fix mobile top margin

--- src/components/WorkPage/WorkPage.tsx
+++ src/components/WorkPage/WorkPage.tsx
@@ -11,10 +11,9 @@ export default function WorkPage() {
       <div className="mx-auto flex h-full w-full max-w-screen-sm flex-col items-center justify-start">
         <NavBar />
         <div className="w-full px-6">
-          <div className="flex h-full flex-col gap-32 pb-72">
+          <div className="flex h-full flex-col gap-32 pb-72 pt-20 md:pt-0">
             <Work />
             <Education />
-            <Projects />
           </div>
         </div>
         <Footer />
