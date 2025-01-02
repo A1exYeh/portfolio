2bb35bd6fb06efaf59857c6025487e7e3c348bb4
Alex Yeh <111445339+AlexYessirski@users.noreply.github.com>
2025-01-02 02:48:15 -0800

    make footer relative

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import '@/index.css';
 export default function Footer() {
   return (
     <>
-      <div className="fixed bottom-0 flex w-full max-w-screen-sm flex-row items-center justify-start px-6 py-12 text-primary">
+      <div className="mt-auto flex w-full max-w-screen-sm flex-row items-center justify-start px-6 py-12 text-primary">
         <div>© 2025 | Alex Yeh</div>
       </div>
     </>
